151e0648732ec8865bd48f101f24d51a5329c4cb
ameeraazli <wanazliwanameera@gmail.com>
2023-08-22 18:01:58 +0800

    successful insertion into postgresql database - code refactor

--- csv_to_postgres.py
+++ csv_to_postgres.py
@@ -15,13 +15,13 @@ def extract_data_from_url(url):
 
   return df
 
-def update_postgres_table(df):
-  connection = psycopg2.connect(
-    host=DB_HOST,
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD
-  )
+def update_postgres_table(df, connection):
+  # connection = psycopg2.connect(
+  #   host=DB_HOST,
+  #   database=DB_NAME,
+  #   user=DB_USER,
+  #   password=DB_PASSWORD
+  # )
 
   cursor = connection.cursor()
 
@@ -31,15 +31,28 @@ def update_postgres_table(df):
 
   connection.commit()
   cursor.close()
-  connection.close()
+  # connection.close()
 
   print("success")
+
+# def run_aggregate_queries(df):
 
 if __name__ == "__main__":
   csv_url = "https://raw.githubusercontent.com/datasets/london-crime/master/data/crime-rates.csv"
   csv_data = extract_data_from_url(csv_url)
   print(csv_data)
 
-  update_postgres_table(csv_data)
+  connection = psycopg2.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+  )
+
+  update_postgres_table(csv_data, connection)
+
+  connection.close()
+
+  print("Connection closed")
 
 

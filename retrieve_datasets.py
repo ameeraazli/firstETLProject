ba09226bd0f87b8fd70dbe446bddbe20f4797e5c
ameeraazli <wanazliwanameera@gmail.com>
2023-08-22 10:42:10 +0800

    include datasets

---
+++ retrieve_datasets.py
@@ -0,0 +1,4 @@
+import opendatasets as od
+
+dataset_url = 'https://www.kaggle.com/ishanshrivastava28/tata-online-retail-dataset'
+od.download('https://www.kaggle.com/ishanshrivastava28/tata-online-retail-dataset')

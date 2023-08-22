caa078a6229810ae28983cb24a35bd256e1984de
ameeraazli <wanazliwanameera@gmail.com>
2023-08-22 10:58:46 +0800

    refactor files, naming and structures

---
+++ csv_to_postgres.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('online_retail_data_set.csv')
+
+print(df.head())
--- retrieve_datasets.py
+++
@@ -1,4 +0,0 @@
-import opendatasets as od
-
-dataset_url = 'https://www.kaggle.com/ishanshrivastava28/tata-online-retail-dataset'
-od.download('https://www.kaggle.com/ishanshrivastava28/tata-online-retail-dataset')
